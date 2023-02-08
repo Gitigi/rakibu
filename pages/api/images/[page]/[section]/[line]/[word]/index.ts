82b024985de79d44a5f5dd0489b68164dd7dad3f
gitigi <stephengitigi@gmail.com>
2023-02-08 19:23:44 +0300

    feat: get word image bbox from db

--- pages/api/images/[page]/[section]/[line]/[word]/index.ts
+++ pages/api/images/[page]/[section]/[line]/[word]/index.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import sharp from 'sharp'
-import fs from 'fs/promises'
+
+import prisma from '@/lib/prisma'
 
 sharp.cache({items: 400, memory: 200})
 
@@ -10,13 +11,22 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const {page, section, line: line_idx, word: word_idx}: any = req.query
-  const data = await fs.readFile(`json_files/${page}.json`, { encoding: 'utf8' });
-  const page_json = JSON.parse(data)
-  let word = page_json['sections'][section][line_idx]['words'][word_idx]
-  let bbox = word['bbox']
-  let width = bbox[1][0] - bbox[0][0]
-  let height = bbox[1][1] - bbox[0][1]
-  let img = await sharp(`page_images/${page}.png`).extract({ width, height, left: bbox[0][0], top: bbox[0][1] }).png().toBuffer()
+  const word: any = await prisma.word.findFirst({
+    select: {
+      bbox: true
+    },
+    where: {
+      page,
+      section,
+      line_index: parseInt(line_idx),
+      index: parseInt(word_idx)
+    }
+  })
+  
+  let [[x1, y1], [x2, y2]] = word['bbox']
+  let width = x2 - x1
+  let height = y2 - y1
+  let img = await sharp(`page_images/${page}.png`).extract({ width, height, left: x1, top: y1 }).png().toBuffer()
   res.setHeader('Content-Type', 'image/png')
   res.status(200).send(img)
 }
