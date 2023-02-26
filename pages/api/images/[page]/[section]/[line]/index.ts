52b6909d2914261a39c648047eeaccdd559af2a3
gitigi <stephengitigi@gmail.com>
2023-02-26 12:00:21 +0300

    fetch line from db in line image

--- pages/api/images/[page]/[section]/[line]/index.ts
+++ pages/api/images/[page]/[section]/[line]/index.ts
@@ -1,18 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import sharp from 'sharp'
-import fs from 'fs/promises'
 
+import prisma from '@/lib/prisma'
+
+sharp.cache({items: 400, memory: 200})
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const {page, section, line: line_idx}: any = req.query
-  const data = await fs.readFile(`json_files/${page}.json`, { encoding: 'utf8' });
-  const page_json = JSON.parse(data)
-  let line = page_json['sections'][section][line_idx]
-  let bbox = line['bbox']
+  const line: any = await prisma.line.findFirst({
+    select: {
+      bbox: true
+    },
+    where: {
+      page,
+      section,
+      index: parseInt(line_idx)
+    }
+  })
+  let bbox = line.bbox
   let left = bbox[0][0] - 20
   let top = bbox[0][1] - 10
   let width = (bbox[1][0] - bbox[0][0]) + 40
@@ -21,4 +36,3 @@ export default async function handler(
   res.setHeader('Content-Type', 'image/png')
   res.status(200).send(img)
 }
-
