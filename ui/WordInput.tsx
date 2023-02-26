2c2caf8db025c8329f94009e39488ab5b90b6aa1
gitigi <stephengitigi@gmail.com>
2023-02-26 13:42:25 +0300

    reduced text size for input in word edit

--- ui/WordInput.tsx
+++ ui/WordInput.tsx
@@ -15,7 +15,7 @@ export default function WordInput( {value, onChange }: any) {
         value={value}
         type="text"
         placeholder="Rakibu"
-        className={`bg-white text-gray-800 font-amiri text-center text-3xl block text-md p-2 h-11 w-full rounded-md border-gray-300 pr-12 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 placeholder:text-lg`}
+        className={`bg-white text-gray-800 font-amiri text-center text-xl block text-md p-2 h-11 w-full rounded-md border-gray-300 pr-12 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 placeholder:text-lg`}
       />
       <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5 group">
         <KeyboardButton inputRef={ref} />
